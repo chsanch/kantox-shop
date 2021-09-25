807c89befe1124d5ecf5b08463117db7a5e87a3f
Christian Sánchez <chsanch@pm.me>
2021-09-25 11:59:03 +0200

    add products list component

--- app/routes/products.js
+++ app/routes/products.js
@@ -2,7 +2,6 @@ import Route from '@ember/routing/route';
 
 export default class ProductsRoute extends Route {
   model() {
-    console.log('here');
     return this.store.findAll('product');
   }
 }
