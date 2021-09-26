ab11eab8cf6f6f64e0892d86aaddaa2fd55875ab
Christian Sánchez <chsanch@pm.me>
2021-09-26 18:26:10 +0200

    remove unused imports

--- app/components/cart.js
+++ app/components/cart.js
@@ -1,6 +1,4 @@
 import Component from '@glimmer/component';
-import { tracked } from '@glimmer/tracking';
-import { action } from '@ember/object';
 import { inject as service } from '@ember/service';
 
 export default class CartComponent extends Component {
